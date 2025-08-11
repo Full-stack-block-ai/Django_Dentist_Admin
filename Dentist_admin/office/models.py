e3ae24a624e47bd2de3ef275932eff6c097e5270
Steve <stephenjmhough@outlook.com>
2025-08-11 11:06:46 +0100

    created and migrated first model

--- Dentist_admin/office/models.py
+++ Dentist_admin/office/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# Define a new class named 'Patient' that inherits from models.Model.
+# 'models.Model' is the base class that gives your class all the
+# functionality of a Django model, like database table creation and
+# object management.
+class  Patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    age = models.IntegerField()
