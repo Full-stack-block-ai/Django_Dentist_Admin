cc2d45a4dd38ca6aac068c0520a6c162e6e744cd
Steve <stephenjmhough@outlook.com>
2025-08-11 14:19:33 +0100

    model practice migrations and validation

--- Dentist_admin/office/models.py
+++ Dentist_admin/office/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 # Define a new class named 'Patient' that inherits from models.Model.
@@ -8,7 +9,8 @@ from django.db import models
 class  Patient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    age = models.IntegerField()
+    age = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(120)])
+    heart_rate = models.IntegerField(default=60,validators=[MinValueValidator(1),MaxValueValidator(250)])
 
     def __str__(self):
 
